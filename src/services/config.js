7943dc9172bebf94ede1e94941b7beb24cb1ac84
Fakytop <prgonzalo94@gmail.com>
2023-07-29 22:04:52 -0300

    se agregan los endpoints para reutilizar luego.

---
+++ src/services/config.js
@@ -0,0 +1,13 @@
+const endpoints = {
+    base_url : 'https://censo.develotion.com/',
+    post_signup : 'usuarios.php',
+    post_login : '/login.php',
+    get_deptos : '/departamentos.php',
+    get_city_id : '/ciudades.php?idDepartamento=',
+    get_cities : '/ciudades.php',
+    get_registered_id : '/personas.php?idUsuario=',
+    post_person : '/personas.php',
+    delete_person : '/personas.php?idCenso=',
+    get_occupations : '/ocupaciones.php',
+    get_all_registered : '/totalCensados.php'
+}
---
+++ src/services/login.jsx
@@ -0,0 +1,5 @@
+import endpoints from './config'
+
+const login = payload => {
+    
+}
